8d68a1e6942494af2cda35f04bac90f55f8dc1c1
Knat-B-Fly <nathan.buckendorf@gmail.com>
2025-09-01 14:19:42 -0500

    modified all.py files

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
@@ -31,24 +28,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        player.update(dt)
-        
+
+        updatable.update(dt)
+
         screen.fill("black")
-        
+
         for obj in drawable:
             obj.draw(screen)
 
         pygame.display.flip()
 
-        # limit the framerate to 60 fps
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
-
-print("Hello from asteroids!")
-print("Starting Asteroids!")
-print(f"Screen width: {SCREEN_WIDTH}")
-print(f"Screen height: {SCREEN_HEIGHT}")
